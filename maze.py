418be590b521599c8de809c68a90a5966df1a9db
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2022-02-28 23:00:12 -0500

    slight speed up, coordinte as input to functions

--- maze.py
+++ maze.py
@@ -5,12 +5,14 @@ import time
 from shapely.geometry import Polygon,Point
 import math
 
+from sympy import true
+
 '''
 Custom data type to store each node
 '''
 class Node:
-    def __init__(self,position = [0,0], parent = [0,0], cost = np.inf,is_not_obstacle = None):
-        self.__attrdict = {"position":np.array(position),"parent":np.array(parent),"cost":cost,"is_not_obstacle":is_not_obstacle }
+    def __init__(self,position = [0,0], parent = None, cost = np.inf,is_visited = None):
+        self.__attrdict = {"position":np.array(position),"parent":np.array(parent),"cost":cost,"is_visited":is_visited }
 
     '''set the position of the state'''
     def set_position(self,position = np.zeros([1,2])):
@@ -19,11 +21,11 @@ class Node:
     def get_position(self):
         return self.__attrdict['position'] 
 
-    def set_is_not_obstacle(self,obstacle = False):
-        self.__attrdict['is_not_obstacle'] = obstacle
+    def set_is_visited(self,is_visited = True):
+        self.__attrdict['is_visited'] = is_visited
     '''Get the position of the current node'''
-    def get_is_not_obstacle(self):
-        return self.__attrdict['is_not_obstacle'] 
+    def get_is_visited(self):
+        return self.__attrdict['is_visited'] 
 
     '''Get the index of the parent'''
     def get_parent(self):
@@ -63,6 +65,7 @@ class Node:
 
 class Maze:
     arrow_obstacle = Polygon([(115,210),(80,180),(105,100),(36,185)]) ##The arrow polygon obstacle
+    circle = Point((300,185)).buffer(40)
     action_set = {'u':[-1,0],'d':[1,0],'l':[0,-1],'r':[0,1],'ul':[-1,-1],'ur':[-1,1],'dl':[1,-1],'dr':[1,1]} ##action set dict, used to append add to the current index
     lim_x = 249## xlimit in terms of array
     lim_y = 399## Y limit in terms of array
@@ -98,14 +101,15 @@ class Maze:
         while True:
             # sort the open list so that lowest cost in at idx 0
             self.__open_list.sort()
-            if(self.__maze[self.start_goal[-1]].get_cost()<self.__open_list[0].get_cost()):
-                break
             # print("Open : ",self.__open_list,list(map(Node.get_cost,self.__open_list)))
             # look around the first node in the open list
+            print(self.__open_list[0].get_position())
             self.look_around(self.__open_list[0].get_position())
             # Add the first node to the closed list and pop it from open list
             self.__close_list.append(self.__open_list[0])
             self.__open_list.pop(0)
+            if(self.__maze[self.start_goal[-1]].get_cost()<self.__open_list[0].get_cost()):
+                break
             # print("Closed List: ",self.__close_list)
             
     def back_track(self):
@@ -125,31 +129,36 @@ class Maze:
         #itr through all the possible actions
         for i in Maze.action_set:
             ## calc the idx of the cell for a particular action
-            temp = list(map(add,points,Maze.action_set[i]))
+            temp = tuple(map(add,points,Maze.action_set[i]))
             ## Check if the idx is with in maze size
             if temp[0]>=0 and temp[0]<=Maze.lim_x and temp[1]>=0 and temp[1]<=Maze.lim_y:
                 ## Check if that element is a Node obj or a None(meaning it is an obstacle)
-                if(self.__maze[tuple(temp)]):
+                if(self.__maze[(temp)]):
                     ### check if the node is an obstacle
-                    if(self.__maze[tuple(temp)].get_is_not_obstacle()):
-                        self.add_to_list(points,temp,self.__cost[i])
+                    # if(self.__maze[tuple(temp)].get_is_not_obstacle()):
+                    self.add_to_list(points,temp,self.__cost[i])
                     ### First time coming here, check if it is an obstacle
-                    elif(self.point_not_in_obstacle(temp)):
-                        self.add_to_list(points,temp,self.__cost[i])
+                    # elif(self.point_not_in_obstacle(temp)):
+                    #     self.add_to_list(points,temp,self.__cost[i])
         
     ## Add to open list and update node parameters
     def add_to_list(self,parent,child,cost):
         ## If the cost is lesser than the existing cost 
-        if(self.__maze[tuple(child)] in self.__close_list):
+        child = tuple(child)
+        parent = tuple(parent)
+        if(self.__maze[child].get_is_visited()):
+            if(self.__maze[child] in self.__close_list):
             # print("Present", tuple(child), self.__close_list.index(self.__maze[tuple(child)]))
-            return
-        if((self.__maze[tuple(parent)].get_cost()+cost)<self.__maze[tuple(child)].get_cost()):
+                return
+        if((self.__maze[parent].get_cost()+cost)<self.__maze[child].get_cost()):
             ## update the cost with the new cost
-            self.__maze[tuple(child)].set_cost(self.__maze[tuple(parent)].get_cost()+cost)
+            self.__maze[child].set_cost(self.__maze[parent].get_cost()+cost)
             ## Set the new parent of the node
-            self.__maze[tuple(child)].set_parent(tuple(parent))
+            self.__maze[child].set_parent(parent)
+            
+            self.__maze[child].set_is_visited()
             ## Add the node to the open list
-            self.__open_list.append(self.__maze[tuple(child)])
+            self.__open_list.append(self.__maze[child])
             # print("open list : ",self.__open_list,list(map(Node.get_cost,self.__open_list)))
         
     ## getter for open list
@@ -164,11 +173,11 @@ class Maze:
     ## Check if a given point is in obstacle, idx is the index
     def point_not_in_obstacle(self,idx):
         ## if the point is in circle or arrow or hexagon return false
-        if((self._is_in_circle(idx) or self._is_in_arrow(idx) or self._is_in_hexagon(idx))):
+        point = self.idx_to_cartisian(idx)
+        if((self._is_in_circle(point) or self._is_in_arrow(point) or self._is_in_hexagon(point))):
             self.__maze[idx] = None
             return False
         ## set the obstacle ness of the node
-        self.__maze[tuple(idx)].set_is_not_obstacle()
         return True
     
     ## HELPER, Converts idx to cartisian
@@ -177,6 +186,7 @@ class Maze:
         xy[0],xy[1] = xy[1],xy[0]
         xy[1] = abs(xy[1]-Maze.lim_x)
         return (xy[0],xy[1])
+    
     ## HELPER, Converts cartisian to idx
     def cartisian_to_idx(self, xy):
         xy = list(xy)
@@ -186,27 +196,48 @@ class Maze:
 
     ## Defines a circle and checks if the idx is within the given circle
     def _is_in_circle(self,idx,radius=40,center=(300,185)):
-        circle = Point(center).buffer(radius)
-        point_geom = Point(self.idx_to_cartisian(idx)).buffer(self.__padding)
-        return circle.intersects(point_geom)
+        point_geom = Point(idx)
+        return Maze.circle.intersects(point_geom)
     
     ## Defines a arrow and checks if the idx is within the given arrow
     def _is_in_arrow(self,idx):
-        point_geom = Point(self.idx_to_cartisian(idx)).buffer(self.__padding)
+        point_geom = Point((idx))
         return Maze.arrow_obstacle.intersects(point_geom)
-        pass
     
     ## Defines a hexagon and checks if the idx is within the given hexagon
     def _is_in_hexagon(self,center=(200,100),side=35):
         return False
         pass
     
+    def update_maze_circle(self):
+        coords = Maze.circle.buffer(5).bounds
+        start = ((math.floor(coords[0]),math.floor(coords[1])))
+        end = ((math.floor(coords[2]),math.floor(coords[3])))
+        
+        x,y = np.ogrid[start[0]:end[0],start[1]:end[1]]
+        
+        x = x.T
+        padding = int(self.__padding)
+        for yi in y[0]:
+            for xi in x[0]:
+                if self._is_in_circle((xi,yi)):
+                    idx,idy = self.cartisian_to_idx((xi,yi))
+                    self.__maze[idx-padding:idx+padding,idy-padding:idy+padding] = None 
+        pass
+    
+    
     def update_maze_arrow(self):
-        coords = Maze.arrow_obstacle.buffer(5).boundary.coords[:]
-        cords = Maze.arrow_obstacle.exterior.coords.xy
-        for i in coords:
-            i = self.cartisian_to_idx(i)
-            f = math.floor(i)
-            c = math.ceil(i)
-            self.__maze[f] = None
-            self.__maze[c] = None
+        coords = Maze.arrow_obstacle.buffer(5).bounds
+        start = ((math.floor(coords[0]),math.floor(coords[1])))
+        end = ((math.floor(coords[2]),math.floor(coords[3])))
+        
+        x,y = np.ogrid[start[0]:end[0],start[1]:end[1]]
+        
+        x = x.T
+        padding = int(self.__padding)
+        for yi in y[0]:
+            for xi in x[0]:
+                if self._is_in_arrow((xi,yi)):
+                    idx,idy = self.cartisian_to_idx((xi,yi))
+                    self.__maze[idx-padding:idx+padding,idy-padding:idy+padding] = None 
+        pass
